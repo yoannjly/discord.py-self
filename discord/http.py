15b9d71df26dfa47d78de666d0ae6cd40a46f048
dolfies <jeyalfie47@gmail.com>
2021-05-09 20:39:48 -0400

    Update http.py

--- discord/http.py
+++ discord/http.py
@@ -109,8 +109,7 @@ class HTTPClient:
         self.proxy_auth = proxy_auth
         self.use_clock = not unsync_clock
 
-        user_agent = 'DiscordBot (https://github.com/Rapptz/discord.py {0}) Python/{1[0]}.{1[1]} aiohttp/{2}'
-        self.user_agent = user_agent.format(__version__, sys.version_info, aiohttp.__version__)
+        self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
 
     def recreate(self):
         if self.__session.closed:
