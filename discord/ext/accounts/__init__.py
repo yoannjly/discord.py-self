e027e5bffcee42ffa3954de39d9b8c4d132011b6
dolfies <jeyalfie47@gmail.com>
2021-07-11 12:20:35 -0400

    Removed useless files

--- discord/ext/accounts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-discord.ext.accounts
-~~~~~~~~~~~~~~~~~~~~~
-
-An extension module to interact with Discord authentication APIs.
-
-:copyright: (c) 2021-present Dolfies
-:license: MIT, see LICENSE for more details.
-"""
