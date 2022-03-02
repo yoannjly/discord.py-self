169354ce6f3f3667de4981b55d5ef04dc19dce15
dolfies <jeyalfie47@gmail.com>
2022-03-01 20:45:32 -0500

    Fix v1.x

--- discord/utils.py
+++ discord/utils.py
@@ -568,7 +568,7 @@ async def _get_build_number(session): # Thank you Discord-S.C.U.M
         build_url = 'https://discord.com/assets/' + re.compile(r'assets/+([a-z0-9]+)\.js').findall(login_page)[-2] + '.js'
         build_request = await session.request('get', build_url, headers={'Accept-Encoding': 'gzip, deflate'}, timeout=10)
         build_file = await build_request.text()
-        build_index = build_file.find('buildNumber') + 14
+        build_index = build_file.find('buildNumber') + 24
         return int(build_file[build_index:build_index + 5])
     except:
         log.warning('Could not fetch client build number.')
